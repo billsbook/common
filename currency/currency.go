f9005df68b18b82da41827efa42bad4e1ce2e879
ali <alikarimi999@protonmail.com>
2022-06-24 19:20:24 +0430

    add GenRandString function

--- currency/currency.go
+++ currency/currency.go
@@ -11,3 +11,13 @@ type Currency struct {
 var (
 	Rial = Currency{"IRAN", "Iranian Rial", "IRR", 364}
 )
+
+// return the currency for the given code
+func GetCurrency(code string) Currency {
+	switch code {
+	case "IRR":
+		return Rial
+	default:
+		return Rial
+	}
+}
---
+++ rand_string.go
@@ -0,0 +1,17 @@
+package common
+
+import (
+	"math/rand"
+	"time"
+)
+
+// Generate a random string of a given length. between a-z, A-Z, 0-9
+func GenRandString(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
